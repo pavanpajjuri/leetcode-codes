f936e8cb9b5d8203e198c8125c4996e866835fee
pavanpajjuri <pavan.pajjuri99@gmail.com>
2025-04-04 02:46:41 -0400

    Adding python files and codes

---
+++ neetcode-150/217-contains-duplicate.py
@@ -0,0 +1,1 @@
+class Solution:    def containsDuplicate(self, nums: List[int]) -> bool:        # nums_dict = {}        # for x in nums:        #     if x in nums_dict:        #         return True        #     else:        #         nums_dict[x] = '1'        # return False        return len(nums) != len(set(nums))
